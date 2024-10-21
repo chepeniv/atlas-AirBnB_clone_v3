a91e17439ba49487f6e67d3266a3871ad788a7ad
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-21 18:14:33 -0500

    Error trying to create user, put in WITH for authentication

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- Ariel create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---Ariel:  create user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_pwd';
+--Ariel:  create user if it doesn't exist; inserted WITH to autheniticate
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH auth_plugin BY 'hbnb_pwd';
 -- Ariel: grant all priveleges only on hbnb_dev_db
 GRANT ALL PRIVELEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Ariel: grant select priveleges only on performance_schema database
