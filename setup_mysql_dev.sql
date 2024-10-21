9a3f945f60b5dd271753602a5dd746f3e8fb4660
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-21 17:53:38 -0500

    forgot semicolon

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
 -- Ariel create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --Ariel:  create user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_pwd';
 -- Ariel: grant all priveleges only on hbnb_dev_db
-GRANT ALL PRIVELEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVELEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Ariel: grant select priveleges only on performance_schema database
-GRANT SELECT ON 'performance_schema' TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
