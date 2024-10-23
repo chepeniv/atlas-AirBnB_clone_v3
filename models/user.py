726fcc342c562cc5e895b9b69b54530279c19bf5
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-23 16:28:21 -0500

    Ariel: updated user file to add class attributes and sqlalchemy imports

--- models/user.py
+++ models/user.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """ user class
 """
+# Ariel- grabbing column and string data types from sqlalchemy 
+# also importing base from file base_model in models folder
 
-import models.base_model as basemodel
+from sqlalchemy import Column, String
+import models.base_model as basemodel, Base
 
-
-class User(basemodel.BaseModel):
+# Ariel - base inherited, kept syntax from chepe
+class User(basemodel.BaseModel, base.BaseModel):
+    # Ariel - class attributes
+    __tablename_ = users
+    
+    # Ariel - using column containing a string with 128 char
+    # email and password cannot be name, where as first and last name can be null
+    
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    
+    ####################
+    # Older code below
+    ####################
+    
     # public class
     # update FileStorage to manange de/serialization of this class
     # update console.py commands to use this class
