112a7c74fbef70e7998b1f3e8757cf28447af27d
chepeniv <jose.olmos@atlasschool.com>
2024-10-23 15:24:16 -0500

    corrected spelling mistakes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,14 @@
 -- Ariel create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
 -- Ariel  create user if it doesn't exist inserted WITH to autheniticate
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH authorization_plugin BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
 -- Ariel grant all priveleges only on hbnb_dev_db
-GRANT ALL PRIVELEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
 -- Ariel grant select priveleges only on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
 -- push priveleges
-FLUSH PRIVELEGES;
+FLUSH PRIVILEGES;
