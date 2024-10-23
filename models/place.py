38e72a4fc4d4c24e4a2caaf0cd5a8734ecbf5a56
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-23 16:55:23 -0500

    Ariel: updated places file, added class attributes, need to figure out if I need to mix, remove or keep code anatomy the wat it is currenly

--- models/place.py
+++ models/place.py
@@ -2,11 +2,15 @@
 """
 Place class that inherits from BaseModel
 """
+# importing mysqlalchemy datatypes
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
+# import Base to inherift from
+from models.base_model import BaseModel, Base
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """
     Place class that inherits from BaseModel
     Public class attributes:
@@ -23,6 +27,28 @@ class Place(BaseModel):
         amenity_ids: list of string -
             empty list: it will be the list of Amenity.id later
     """
+    # adding class attributes along with column, string, foreignkey
+    # and whether or not it can be null
+    ##########
+    # New code -Ariel
+    ##########
+    
+    __tablename__ = places
+    
+    city_id = Column(String(60), ForeignKey(cities.id), nullable=False)
+    user_id = Column(String(60), ForeignKey(users.id), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_of_rooms = Column(Integer, default=0, nullable=False)
+    number_of_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullabe=True)
+    longitude = Column(Float, nullable=True)
+    
+    ##########
+    #Old code
+    ##########
+    
     def __init__(self, *args, **kwargs):
         if kwargs:
             super().__init__(*args, **kwargs)
