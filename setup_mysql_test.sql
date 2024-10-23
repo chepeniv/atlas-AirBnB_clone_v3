3f940d0e6d0405609a6dc5f44eaf093e350b2a10
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-23 15:46:03 -0500

    Ariel: I had privileges and an extra on, fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,10 +8,7 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- grant priveleges for user on schema db
-GRANT SELECT ON PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- push priveleges
 FLUSH PRIVILEGES;
-
--- show grants for user
-SHOW GRANTS FOR 'hbnb_test'@'localhost';
