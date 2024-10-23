d55a83e169f1988055fbfcb64607301db50003c7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-23 15:38:08 -0500

    Ariel: created test for file 3. test database,

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Create db if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- grant privileges foruser on test db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- grant priveleges for user on schema db
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- push priveleges
+FLUSH PRIVILEGES;
