51c1d600b571ff39344020ba07672923d9dd337d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-23 14:16:42 -0500

    erased priveleges key

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --Ariel:  create user if it doesn't exist; inserted WITH to autheniticate
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Ariel: grant all priveleges only on hbnb_dev_db
-GRANT ALL PRIVELEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Ariel: grant select priveleges only on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
