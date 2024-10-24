d18ab12e757fc92a768b3b4fac212152eca6a5a6
chepeniv <jose.olmos@atlasschool.com>
2024-10-24 05:41:35 -0500

    changed how environment variable isa extracted

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 import os
 
 
-storage_type = os.environ['HBNB_TYPE_STORAGE']
+storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
 
 if storage_type == 'db':
     import models.engine.DBStorage as fs
