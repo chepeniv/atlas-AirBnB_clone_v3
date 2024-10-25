d66d3995d570e1da876bba9496b62df0f28a6aa9
chepeniv <jose.olmos@atlasschool.com>
2024-10-25 05:35:24 -0500

    exported shell vars

--- tests/sh_tests/task6-test.sh
+++ tests/sh_tests/task6-test.sh
@@ -2,13 +2,13 @@
 
 # rm json.file;
 
-cat setup_mysql_dev.sql | mysql;
+export HBNB_MYSQL_USER=hbnb_dev
+export HBNB_MYSQL_PWD=hbnb_dev_pwd
+export HBNB_MYSQL_HOST=localhost
+export HBNB_MYSQL_DB=hbnb_dev_db
+export HBNB_TYPE_STORAGE=db
 
-HBNB_MYSQL_USER=hbnb_dev
-HBNB_MYSQL_PWD=hbnb_dev_pwd
-HBNB_MYSQL_HOST=localhost
-HBNB_MYSQL_DB=hbnb_dev_db
-HBNB_TYPE_STORAGE=db
+cat setup_mysql_dev.sql | mysql;
 
 tests/console_cmds.sh | ./console.py &> tests/console_err
 
