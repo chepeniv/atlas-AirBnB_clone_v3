903d35ee4364c62c60026696be4b0b4fd8d6b5de
chepeniv <jose.olmos@atlasschool.com>
2024-10-26 06:11:22 -0500

    updated gitignore and preliminary shell script

--- run_console.sh
+++ run_console.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-# echo "select valid mode: 'dev' or 'test' :"
-export HBNB_DB_MODE=dev
-export HBNB_ROOT=~/Learning/AtlasSchool/Sandbox/atlas-AirBnB_clone_v2/
-export HBNB_SH_TEST_DIR=$HBNB_ROOT/tests/sh_tests/
-export HBNB_MYSQL_USER=hbnb_$HBNB_DB_MODE
-export HBNB_MYSQL_PWD=hbnb_$HBNB_DB_MODE\_pwd
-export HBNB_MYSQL_HOST=localhost
-export HBNB_MYSQL_DB=hbnb_$HBNB_DB_MODE\_db
-export HBNB_TYPE_STORAGE=db
-
+(
+export HBNB_DB_MODE=dev;
+export HBNB_ROOT=~/Learning/AtlasSchool/Sandbox/atlas-AirBnB_clone_v2/;
+export HBNB_SH_TEST_DIR=$HBNB_ROOT/tests/sh_tests/;
+export HBNB_MYSQL_USER=hbnb_$HBNB_DB_MODE;
+export HBNB_MYSQL_PWD=hbnb_$HBNB_DB_MODE\_pwd;
+export HBNB_MYSQL_HOST=localhost;
+export HBNB_MYSQL_DB=hbnb_$HBNB_DB_MODE\_db;
+export HBNB_TYPE_STORAGE=db;
 sudo python3 console.py
+)
 
