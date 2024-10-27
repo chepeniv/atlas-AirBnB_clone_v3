620ad95808e01a69cf7f6c26059423a294895640
chepeniv <jose.olmos@atlasschool.com>
2024-10-26 19:48:34 -0500

    made run_console.sh more portable

--- run_console.sh
+++ run_console.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 export HBNB_DB_MODE=dev;
-export HBNB_ROOT=~/Learning/AtlasSchool/Sandbox/atlas-AirBnB_clone_v2/;
-export HBNB_SH_TEST_DIR=$HBNB_ROOT/tests/sh_tests/;
+export HBNB_SH_TEST_DIR=$PWD/tests/sh_tests/;
 
 HBNB_MYSQL_USER=hbnb_$HBNB_DB_MODE HBNB_MYSQL_PWD=hbnb_$HBNB_DB_MODE\_pwd HBNB_MYSQL_HOST=localhost; HBNB_MYSQL_DB=hbnb_$HBNB_DB_MODE\_db HBNB_TYPE_STORAGE=db ./console.py
 
