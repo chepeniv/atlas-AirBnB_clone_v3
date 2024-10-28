5b6db7a450a487678b9fb154e0b15bf7fc23a8f3
chepeniv <jose.olmos@atlasschool.com>
2024-10-28 01:25:56 -0500

    added and updated .sh scripts. read them. use them.

--- run_console.sh
+++ run_console.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#CHEPE:
+#make sure you're in the root project directory before running
+#command:
+#	sudo sh run_console.sh
+
+#uncomment to automatically clear up database each time
+#cat dev_db_setup.sql | mysql
+
 clear;
 export HBNB_DB_MODE=dev;
 export HBNB_SH_TEST_DIR=$PWD/tests/sh_tests/;
 
-HBNB_MYSQL_USER=hbnb_$HBNB_DB_MODE HBNB_MYSQL_PWD=hbnb_$HBNB_DB_MODE\_pwd HBNB_MYSQL_HOST=localhost; HBNB_MYSQL_DB=hbnb_$HBNB_DB_MODE\_db HBNB_TYPE_STORAGE=db ./console.py
+HBNB_MYSQL_USER=hbnb_$HBNB_DB_MODE HBNB_MYSQL_PWD=hbnb_$HBNB_DB_MODE\_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_$HBNB_DB_MODE\_db HBNB_TYPE_STORAGE=db ./console.py
 
