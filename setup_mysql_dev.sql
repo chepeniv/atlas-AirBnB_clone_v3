3b1770d7275901e425e73fcc3b11ebbba0f15490
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-10-28 22:42:20 -0500

    Ariel: removed with command in comments, removed e from privileges and added i.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,14 @@
--- Ariel create database if it doesn't exist
+-- create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Ariel  create user if it doesn't exist inserted WITH to autheniticate
+-- create user if it doesn't exist attatched with password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Ariel grant all priveleges only on hbnb_dev_db
+-- grant all privileges only on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Ariel grant select priveleges only on performance_schema database
+-- grant select privileges only on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
--- push priveleges
+-- push privileges
 FLUSH PRIVILEGES;
