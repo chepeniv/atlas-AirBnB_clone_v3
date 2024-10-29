3058df6cfe9b0cc66fd0c54faeb9f9d8506fe814
chepeniv <jose.olmos@atlasschool.com>
2024-10-28 19:11:33 -0500

    added helpful output messages to determine working setup

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,11 @@
 import os
 
 storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
+db = os.environ.get('HBNB_ENV', 'test')
 
+print("working in {} storage mode".format(storage_type))
 if storage_type == 'db':
+    print("using database {}".format(db))
     from models.engine.db_storage import DBStorage as Storage
 else:
     from models.engine.file_storage import FileStorage as Storage
