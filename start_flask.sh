b31b56a42fa5a4fb2170149dc6a842b91f267770
chepeniv <jose.olmos@atlasschool.com>
2024-11-02 01:03:51 -0500

    added a dynamic route function that echos the url as text

--- start_flask.sh
+++ start_flask.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 file_name=$(basename $1 .py);
-echo "file name : $file_name"
+#echo "file name : $file_name"
 
-python3 -m web_flask.$file_name && \
-curl 0.0.0.0:5000 ; echo "" | cat -e
+python3 -m web_flask.$file_name
+#curl 0.0.0.0:5000 ; echo "" | cat -e
