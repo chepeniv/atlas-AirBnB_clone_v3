ac1a52bc85d19474d1652ae493afe8d527d4082d
chepeniv <jose.olmos@atlasschool.com>
2024-11-02 01:28:17 -0500

    added another dynamic route but with a default value

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 '''
-task 2 - c is fun
+task 3 - python is cool
 
 - application should be listening to 0.0.0.0:5000
-- use the option 'strict_slashes=False' within the route definition
+- use the option 'strict_slashes=False' within the route definitions
 - route '/' should display 'Hello HBNB!'
 - route '/hbnb' should display 'HBNB'
 - route '/c/<text>' should display 'C' followed by the value of <text>
     (replace underscores with spaces)
+- route '/python/<text>' should display 'Python' followed by the value of <text>
+    with default value "is cool" (replace underscores with spaces)
 '''
 
 from flask import Flask
@@ -39,6 +41,16 @@ def c_text(text):
     '''
     text = text.replace("_", " ")
     return f"C {escape(text)}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is_cool"):
+    '''
+    process the given url and serve the resulting text on route /python/<text>
+    '''
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
 
 
 if __name__ == '__main__':
