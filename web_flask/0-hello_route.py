5c778aee4c52ce9fd26738be6ca54a4d1a9adcbe
chepeniv <jose.olmos@atlasschool.com>
2024-11-02 00:03:19 -0500

    style edits

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,16 +9,16 @@ task 0 - hello flask!
 
 from flask import Flask
 
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
     '''
     output text, upon request, to 0.0.0.0:5000
     '''
-    return("Hello HBNB!")
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
