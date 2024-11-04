63e903fae71b310039c9aec21160a640ef3bf2c0
chepeniv <jose.olmos@atlasschool.com>
2024-11-03 20:16:51 -0600

    cleaned up and setup starting files

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -11,35 +11,12 @@ from flask import Flask, abort, render_template
 
 app = Flask(__name__)
 
-# /hbnb_filters
-# duplicate 6-index.html into templates/10-hbnb_filters.html
-#       replace content of H4 under each filter title (H3 states and H3
-#       amenities)by &nbsp;
-# into web_flask/static/styles
-#       copy 3-footer.css, 3-header.css, 4-common.css, 6-filters.css
-#       update class .popover in 6-filters.css
-#           enable scrolling and set max height of 300px
-# into web_flask/static/images
-#       copy icon.png, logo.png,
-# all loaded objects (states, cities, and amenities) will be loaded from
-#       DBStorage and must be sorted alphabetically by name
-@app.route("/states", strict_slashes=False)
-def all_states():
-    '''
-    serves a template that displays all State objects by name in alphabetical
-    order
-    '''
-    states = storage.all(State).values()
-    states = list(states)
-    if len(states) > 0:
-        return render_template("9-states.html", states=states, single=False)
-    else:
-        abort(404)
-
-
-# determine how a state with no cities should be displayed
-@app.route("/states/<idnum>", strict_slashes=False)
-def state_by_id(idnum):
+# update class .popover in 6-filters.css
+#       enable scrolling and set max height of 300px
+# all loaded objects will be loaded from DBStorage and must be
+#       sorted alphabetically by name
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
     '''
     serves a template that displays a single State denoted by its id as well as
     all of its cities in alphabetical order
