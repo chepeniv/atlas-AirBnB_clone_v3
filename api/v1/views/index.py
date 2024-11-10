9feb7074f82117ee9060b574a3987b8e99d0b3bd
chepeniv <jose.olmos@atlasschool.com>
2024-11-10 04:39:07 -0600

    returning python dict instead of str

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,6 @@ definitions of routes for the app_views blueprint
 '''
 
 from api.v1.views import app_views
-from json import dumps
 
 
 @app_views.route('/status')
@@ -13,5 +12,5 @@ def status():
 	return a json string denoting the status of the blueprint
 	'''
 	message = {"status": "OK"}
-	message = dumps(message)
+	# python object is automatically return as json object
 	return message
