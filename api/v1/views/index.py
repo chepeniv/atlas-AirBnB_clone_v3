474de13f2645817efe47b2a9624d5f4aa7ef1f3d
chepeniv <jose.olmos@atlasschool.com>
2024-11-11 08:00:31 -0600

    removed probing code. checker is safeguarded against it

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,7 @@
 definitions of routes for the app_views blueprint
 '''
 
-# import os
-from api.v1.views import app_views, storage, valid_models, storage_type, db
+from api.v1.views import app_views, storage, valid_models
 
 
 @app_views.route('/status')
@@ -13,10 +12,8 @@ def status():
     return a json string denoting the status of the blueprint
     '''
     message = {"status": "OK"}
-    # python objects are automatically returned as json by flask
     return message
 
-# route /api/v1/stats
 @app_views.route('/stats')
 def count_each_model():
     '''
@@ -26,10 +23,4 @@ def count_each_model():
     for name, model in valid_models().items():
         count = storage.count(model)
         counts.update({name: count})
-    # intranet debugging probe
-    # environ_items = os.environ.items()
-    # environ_items = list(environ_keys)
-    # counts.update({'environ_items': environ_items})
-    counts.update({'database': db})
-    counts.update({'storage_type': storage_type})
     return counts
