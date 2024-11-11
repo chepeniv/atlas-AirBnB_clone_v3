870c90104d45e2f24702a38a02b7a7832cd5d137
chepeniv <jose.olmos@atlasschool.com>
2024-11-11 05:42:46 -0600

    updated and added working comments as well as def count_each()

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,23 @@ def status():
     message = {"status": "OK"}
     # python objects are automatically returned as json by flask
     return message
+
+# route /api/v1/stats
+@app_views.route('/stats')
+def count_each():
+    '''
+    retrieve and return the number of each object by type
+    '''
+
+    # use storage.count()
+    # json output example :
+    # {
+    # "amenities": 47,
+    # "cities": 36,
+    # "places": 154,
+    # "reviews": 718,
+    # "states": 27,
+    # "users": 31
+    # }
+
+    return "count"
