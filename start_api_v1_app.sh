08be3f3b60ac50e0d48d682d7763ce10ca4592bf
chepeniv <jose.olmos@atlasschool.com>
2024-11-11 07:03:41 -0600

    updated .sh scripts and added more matches to .gitignore

--- start_api_v1_app.sh
+++ start_api_v1_app.sh
@@ -4,13 +4,14 @@
 echo "be sure to run this and all scripts from within their directory"
 
 # HBNB_MODELS_ROOT=../models \
-HBNB_MYSQL_USER=hbnb_dev \
-HBNB_MYSQL_PWD=hbnb_dev_pwd \
-HBNB_MYSQL_HOST=localhost \
-HBNB_MYSQL_DB=hbnb_dev_db \
-HBNB_TYPE_STORAGE=db \
+HBNB_ENV=dev \
 HBNB_API_HOST=0.0.0.0 \
 HBNB_API_PORT=5000 \
+HBNB_MYSQL_DB=hbnb_dev_db \
+HBNB_MYSQL_HOST=localhost \
+HBNB_MYSQL_PWD=hbnb_dev_pwd \
+HBNB_MYSQL_USER=hbnb_dev \
+HBNB_TYPE_STORAGE=db \
 python3 -m api.v1.app
 
 #curl -X GET 0.0.0.0:5000/api/v1/status
